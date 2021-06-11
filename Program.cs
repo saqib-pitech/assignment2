3080972fca2722cac71e42bf097b880c6b68be2b
saqib-pitech <85691240+saqib-pitech@users.noreply.github.com>
2021-06-11 18:15:38 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@ namespace Assignment2
             new int[] { 6,7,8,9 },
             new int[] { 10,11 }
         };
+        /*static int[2][,] jaggedArray2 = new int[2][,]
+        {
+            new int[,] {{1,2},{3,4}},
+            new int[,] {{5,6},{7,8}}
+        };*/
+
         public static void Test()
         {
             for (int i = 0; i < jaggedArray.Length; i++)
@@ -26,16 +32,37 @@ namespace Assignment2
         }
     }
 
-    class StringBuilderDemo
-    {
-        //StringBuilder
-    }
+    
 
     class Program
     {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T t;
+            t = a;
+            a = b;
+            b = t;
+        }
         static void Main(string[] args)
         {
             JaggedArray.Test();
+
+            #region string builder
+            StringBuilder sb = new StringBuilder("Engine");
+            sb.Append(new char[] { 'e', 'r' });
+            sb.Insert(0, "senior ");
+            sb.AppendFormat(" - {0}{1}", "pi", 'T');
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+            sb.Replace('s', 'S');
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+            #endregion
+
+            #region Generic Swap
+                int a = 1, b = 2;
+                Swap<int>(ref a, ref b);
+                Console.WriteLine($"{a},{b}");
+            #endregion
+            s
             Console.Read();
         }
     }

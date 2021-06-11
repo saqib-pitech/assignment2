d482e53e32b4fc69e6325759f03d5eaa2a5b68de
saqib-pitech <85691240+saqib-pitech@users.noreply.github.com>
2021-06-11 17:33:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class JaggedArray
+    {
+        static int[][] jaggedArray = new int[][]
+        {
+            new int[] { 1,2,3,4,5 },
+            new int[] { 6,7,8,9 },
+            new int[] { 10,11 }
+        };
+        public static void Test()
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.WriteLine(jaggedArray[i][j]);
+                }
+            }
+        }
+    }
+
+    class StringBuilderDemo
+    {
+        //StringBuilder
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JaggedArray.Test();
+            Console.Read();
+        }
+    }
+}
